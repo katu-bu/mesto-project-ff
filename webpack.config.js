977aa3c3c052d09f8e96f9568661f3e97f88546a
katu_bu <ekaterinabudnikova@yandex.ru>
2024-12-15 17:08:42 +0100

    proj6 amended after review

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,6 @@ module.exports = {
         exclude: "/node_modules/",
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
-      },
-      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
@@ -40,7 +36,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
           filename: "images/[name].[hash][ext]",
